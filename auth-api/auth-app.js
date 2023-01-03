b90555c09a71fa7aab7dff0e08fda8eb1934f63b
BrCroner <br.croner@gmail.com>
2023-01-03 17:03:31 -0300

    Primeiro commit

---
+++ auth-api/auth-app.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const appzip = require('appmetrics-zipkin')({
+    host: 'process.env.ZIPKIN_SERVICE_HOST',
+    port: 'process.env.ZIPKIN_SERVICE_PORT',
+    serviceName:'zipkin-auth',
+});
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.get('/verify-token/:token', (req, res) => {
+    const token = req.params.token;
+
+    if (token === 'abc') {
+        return res.status(200).json({ message: 'Token válido.', uid: 'u1' });
+    }
+    res.status(401).json({ message: 'Token inválido.' });
+});
+
+app.get('/token/:hashedPassword/:enteredPassword', (req, res) => {
+    const hashedPassword = req.params.hashedPassword;
+    const enteredPassword = req.params.enteredPassword;
+
+    if (hashedPassword === enteredPassword + '_hash') {
+        const token = 'abc';
+        return res.status(200).json({ message: 'Token criado.', token: token });
+    }
+    res.status(401).json({ message: 'Senhas não coincidem.' });
+});
+
+app.get('/hashed-password/:password', (req, res) => {
+    const enteredPassword = req.params.password;
+    const hashedPassword = enteredPassword + '_hash';
+    res.status(200).json({ hashedPassword: hashedPassword });
+});
+
+app.listen(80);
---
+++ tasks-api/tasks-app.js
@@ -0,0 +1,63 @@
+import path from 'path';
+import fs from 'fs';
+import express from 'express';
+import bodyParser from 'body-parser';
+import axios from 'axios';
+
+const filePath = path.join(__dirname, process.env.TASKS_FOLDER, 'tasks.txt');
+
+const app = express();
+
+app.use(bodyParser.json());
+
+const extractAndVerifyToken = async (headers) => {
+    if (!headers.authorization) {
+        throw new Error('Token não foi informado.');
+    }
+    const token = headers.authorization.split(' ')[1];
+
+    const response = await axios.get(`http://${process.env.AUTH_ADDRESS}/verify-token/` + token);
+    return response.data.uid;
+};
+
+app.get('/tasks', async (req, res) => {
+    try {
+        const uid = await extractAndVerifyToken(req.headers);
+        fs.readFile(filePath, (err, data) => {
+            if (err) {
+                console.log(err);
+                return res.status(500).json({ message: 'Carregamento das terefas falhou.' });
+            }
+            const strData = data.toString();
+            const entries = strData.split('TASK_SPLIT');
+            entries.pop();
+            console.log(entries);
+            const tasks = entries.map((json) => JSON.parse(json));
+            res.status(200).json({ message: 'Tarefas atualizadas', tasks: tasks });
+        });
+    } catch (err) {
+        console.log(err);
+        return res.status(401).json({ message: err.message || 'Falha ao carregar tarefas.' });
+    }
+});
+
+app.post('/tasks', async (req, res) => {
+    try {
+        const uid = await extractAndVerifyToken(req.headers);
+        const text = req.body.text;
+        const title = req.body.title;
+        const task = { title, text };
+        const jsonTask = JSON.stringify(task);
+        fs.appendFile(filePath, jsonTask + 'TASK_SPLIT', (err) => {
+            if (err) {
+                console.log(err);
+                return res.status(500).json({ message: 'Falha ao armazenar tarefas.' });
+            }
+            res.status(201).json({ message: 'Tarefa armazenada.', createdTask: task });
+        });
+    } catch (err) {
+        return res.status(401).json({ message: 'Não foi possível verificar token.' });
+    }
+});
+
+app.listen(8000);
